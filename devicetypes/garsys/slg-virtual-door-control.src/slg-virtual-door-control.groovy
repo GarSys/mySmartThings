da4577131b5ff69a36b96efb40e9516e8fee475b
Scott Garver <garversystems@gmail.com>
2018-07-11 23:45:00 -0500

    Initial attempt

---
+++ devicetypes/garsys/slg-virtual-door-control.src/slg-virtual-door-control.groovy
@@ -0,0 +1,24 @@
+metadata {
+    definition(name: "SLG Virtual Door Control", namespace: "GarSys", author: "Scott Garver") {
+		capability "Door Control"
+    }
+	tiles {
+		multiAttributeTile(name:"Door", type: "generic", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.door", key: "PRIMARY_CONTROL"){
+				attributeState "closed", label: '${name}', action: "door control.open", icon: "st.doors.garage.garage-closed", backgroundColor: "#ffffff", nextstate:"open"
+				attributeState "open", label: '${name}', action: "door control.close", icon: "st.doors.garage.garage-open", backgroundColor: "#79b821", nextstate:"closed"
+			}
+		}
+		main "Door"
+		details(["Door","open","close"])
+	}
+}
+def open(){
+	log.debug "Open"
+	sendEvent(name: "Door", value: "open");
+}
+
+def close(){
+	log.debug "Close"
+	sendEvent(name: "Door", value: "close");
+}
